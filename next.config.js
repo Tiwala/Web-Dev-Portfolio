8d0e1f27ddc4803824802a90664d914a2feb7121
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2025-02-18 15:50:47 -0500

    changed nextconfig.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
+  // images: {
+  //   unoptimized: true
+  // },
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  basePath: isProd ? '/webdev-portfolio' : '',
-  assetPrefix: isProd ? '/webdev-portfolio/' : '',
+  // basePath: isProd ? '/webdev-portfolio' : '',
+  // assetPrefix: isProd ? '/webdev-portfolio/' : '',
 }
 
 module.exports = nextConfig

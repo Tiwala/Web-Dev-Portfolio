fcc407f0215c8ccbdc1d30401684929f947c4b36
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2025-02-18 13:57:24 -0500

    changed mainbody.js

--- components/MainBody/MainBody.js
+++ components/MainBody/MainBody.js
@@ -11,10 +11,10 @@ const MainBody = () => {
         <div className={styles.name}>Gerard San Juan</div>
         <div className={styles.normalText}>
           <h2>Software Engineer, Web Developer, and Army Veteran.</h2>
-          <p className={styles.paragraph}>I am a software engineer specialized in creating applications that make people&apos;s lives easier.
+          <p className={styles.paragraph}>I am a software engineer specialized in creating applications that provide value to organizations, big and small.
           <br/>
           <br/>
-          If I made it, I made it with you in mind.
+          Whether it&apos;s to sell candy, or to save the world, we will create great things together, with style.
           </p>
         </div>
     </div>

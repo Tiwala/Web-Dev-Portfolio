00150f98895bcd9f6093a572d260713cd20f0565
Gerard San Juan <49268224+Tiwala@users.noreply.github.com>
2025-02-18 14:39:34 -0500

    updated nextjs.yml and next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
+  basePath: '/webdev-portfolio', // Replace with your repository name
+  assetPrefix: '/webdev-portfolio/', // Replace with your repository name
 }
 
 module.exports = nextConfig
